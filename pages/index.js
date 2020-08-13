22369aeb50746487d2abecd197b4519b300a1cb2
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-13 13:55:55 -0300

    feature: Created users page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 const Home = () => (
-    <h1>Hello World!</h1>
+    <>
+        <h1>Hello World!</h1>
+        <Link href="/users">
+            <a>Usuários</a>
+        </Link>
+    </>
 );
 
 export default Home;
